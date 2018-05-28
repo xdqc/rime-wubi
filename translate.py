5a9a942d68fd59620ee39aabb46d379a2eff7ba9
xdqc <qichen.d@outlook.com>
2018-05-28 17:26:01 +1200

    add websearch corpus en-encoding

--- translate.py
+++ translate.py
@@ -33,19 +33,46 @@ def getTranslate(word):
 
 def makeEnEncoding():
     words = []
-    with open('encoding_zh.txt', mode='r', encoding='utf8') as r:
+    with open('corpus_zh.txt', mode='r', encoding='utf8') as r:
         for line in r.readlines():
             words.append(tuple(line.split('\t')))
 
-    with open('translated', 'r') as r:
+    with open('corpus_en_w.txt', 'r') as r:
         i=0
         for line in r.readlines():
-            words[i] = tuple([words[i][0], line.lower().strip(), words[i][2].strip()])
+            words[i] = tuple([words[i][0], line.lower().strip(), words[i][1].strip()])
             i+=1
 
-    with open('encoding_en.txt', 'w', encoding="utf8") as w:
+    with open('corpus_en.txt', 'w', encoding="utf8") as w:
         for word in words:
             w.write('\t'.join(list(word))+'\n')
+
+
+def sieveSetSubstractionWords():
+    existingWords = []
+    newCorpusWords = {}
+
+    with open('encoding_zh.txt', mode='r', encoding='utf8') as r:
+        for line in r.readlines():
+            existingWords.append(line.split('\t')[0])
+
+    with open('ctx.csv', mode='r', encoding='utf8') as r:
+        for line in r.readlines():
+            word,count = line.split(',')[0], int(line.split(',')[1])
+            if len(word) > 1 and count > 1000:
+                newCorpusWords[word] = count
     
+    newWords = list(newCorpusWords.keys() - set(existingWords))
+    print(len(newWords))
+    newCorpus = {}
+    for word in newWords:
+        newCorpus[word] = newCorpusWords[word]
+
+    newWords = sorted(newCorpus.items(), key=lambda x: x[1], reverse=True)
+
+    with open('corpus_zh.txt', 'w', encoding='utf8') as file:
+        for word in newWords:
+            file.write(word[0]+'\t'+str(word[1]*100)+'\n')
+
 
 makeEnEncoding()
